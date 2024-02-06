7703ee0322b67c102fa44b49af1c6bc1f3459866
Amal221200 <amalmurikkoli2000@gmail.com>
2024-02-06 11:32:12 +0530

    tailwind css

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,10 +1,9 @@
 
-
 function App() {
 
   return (
     <>
-      
+      <h1 className="text-center font-bold mx-6">Hello {["Amal", "Sunny", "Harjeet"].reduce((total, name) => `${total} ${name}`)}</h1>
     </>
   )
 }
